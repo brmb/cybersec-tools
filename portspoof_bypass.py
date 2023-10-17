32c01ca0cc9fb6ea104334295666122685666c9b
Lukas B <lukas.bacinskas@hostinger.com>
2023-10-17 20:37:57 +0300

    tools: Add portspoof bypass

---
+++ portspoof_bypass.py
@@ -0,0 +1,38 @@
+import socket
+
+def scan_port(host, port, protocol):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)
+        s.connect((host, port))
+
+        if protocol == "FTP":
+            s.send(b'USER anonymous\r\n')
+            s.recv(1024)
+            s.send(b'PASS anonymous\r\n')
+            response = s.recv(1024)
+            if "Login" in response.decode('UTF-8') or "successful" in response.decode('UTF-8'):
+                print(f"[+] {port}/tcp {protocol} [Open]")
+        elif protocol == "HTTP":
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.settimeout(5)
+            client.connect((host, port))
+            request = f"GET / HTTP/1.1\r\nHost: {host}\r\n\r\n"
+            client.send(request.encode())
+            response = client.recv(4096)
+            print(f"Received Response from Port {port}:\n{response.decode('UTF-8')}")
+            if b"html" in response:
+                print(f"[+] {port}/tcp {protocol} [Open]")
+            client.close()
+
+        s.close()
+    except (ConnectionRefusedError, socket.timeout):
+        print(f"[-] {port}/tcp {protocol} [Closed]")
+
+if __name__ == "__main__":
+    target = input("Enter Target IP: ")
+    print("[-] Start Port scanning on", target)
+
+    ports_to_scan = [21, 80]  # Add more ports as needed
+    for port in ports_to_scan:
+        scan_port(target, port, "FTP" if port == 21 else "HTTP")
